a1038f081f137f2d661505ae40f6a2c75f7be495
Acambaro <galeana508@hotmail.com>
2016-02-09 11:05:50 -0800

    Add index.html and app.js to repo

---
+++ app.js
@@ -0,0 +1,17 @@
+// JS file for Feb 8 class demo
+
+
+var userName = prompt ('what is you name?')
+console.log ('The user says their name is' + userName);
+
+
+alert('meow meow meow'+ userName + '!!!!!');
+
+var userName = prompt ('What is your game');
+
+alert('So. ' + userName + ' I heard you like ' + userName);
+
+var loveMate = confirm (userName +'Do you like me?');
+
+alert ('Does' + userName + 'love me?' + userMate);
+console.log(userGame);
